a786780fcb1ce21d41db2b5b6e3f43e83535981d
Rafael Alves <rafael.alves09i@hotmail.com>
2025-10-12 09:41:00 -0300

    Segundo exercício em C

---
+++ exerc2.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(){
+
+    printf("Treinamento em Programacao.\nLinguagem C.\n"); /*a)*/
+
+    printf("\n"); /*separar a do b*/
+
+
+    printf("Treinamento em");
+    printf("Programacao.\n");
+    printf("Liguagem C.\n");
+    return 0;
+}
